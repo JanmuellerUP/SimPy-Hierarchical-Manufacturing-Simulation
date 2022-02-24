24360fabc9df933dbc66542b8e697ea02da3c96c
JanmuellerUP <57217430+JanmuellerUP@users.noreply.github.com>
2022-02-24 14:51:16 +0100

    Add files via upload

--- RewardLayer.py
+++ RewardLayer.py
@@ -1,4 +1,24 @@
 import pandas as pd
 
+
 def evaluate_choice(choice):
+
+    # Penalty criteria for forbidden choices
+    criteria = [
+        choice["order"] == 0,
+        choice["locked"] == 2,
+        choice["picked_up"] == 1,
+        choice["in_m_input"] == 1,
+        choice["in_m"] == 1,
+        choice["processing"] == 1,
+        choice["in_same_cell"] == 0,
+        choice["_destination"] == -1
+    ]
+
+    penalty = -1000 * sum(criteria)
+
+    return penalty
+
+
+def reward_action():
     return 0
